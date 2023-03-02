0fc1626eea075ea08cfe1d3899f55d90fc67b52a
leylaoczak <tyekhmurova@gmail.com>
2023-03-02 18:18:13 +0200

    fourth commit

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+
+    {
+    id: 1,
+    item: "Lipstick",
+    image: "https://cdn.glitch.global/7778ae7d-3ffe-4d82-8131-4e9dc22235e5/pexels-meys-lhwa-4699180.jpg?v=1677759599082",
+    },
+
+    {
+    id: 2,
+    item: "Lip Pencil",
+    image: "https://cdn.glitch.global/7778ae7d-3ffe-4d82-8131-4e9dc22235e5/pexels-%F0%9D%90%95%F0%9D%90%9E%F0%9D%90%A7%F0%9D%90%AE%F0%9D%90%AC-%F0%9D%90%87%F0%9D%90%83-%F0%9D%90%8C%F0%9D%90%9A%F0%9D%90%A4%F0%9D%90%9E-%F0%9D%90%AE%F0%9D%90%A9-%26-%F0%9D%90%8F%F0%9D%90%9E%F0%9D%90%AB%F0%9D%90%9F%F0%9D%90%AE%F0%9D%90%A6%F0%9D%90%9E-2535928.jpg?v=1677759578759",
+    },
+
+    {
+    id: 3,
+    item: "Face Powder",
+    image: "https://cdn.glitch.global/7778ae7d-3ffe-4d82-8131-4e9dc22235e5/pexels-%F0%9D%90%95%F0%9D%90%9E%F0%9D%90%A7%F0%9D%90%AE%F0%9D%90%AC-%F0%9D%90%87%F0%9D%90%83-%F0%9D%90%8C%F0%9D%90%9A%F0%9D%90%A4%F0%9D%90%9E-%F0%9D%90%AE%F0%9D%90%A9-%26-%F0%9D%90%8F%F0%9D%90%9E%F0%9D%90%AB%F0%9D%90%9F%F0%9D%90%AE%F0%9D%90%A6%F0%9D%90%9E-2688991.jpg?v=1677759591082",
+    },
+
+    {
+    id: 4,
+    item: "Lash Extreme Volume",
+    image: "https://cdn.glitch.global/7778ae7d-3ffe-4d82-8131-4e9dc22235e5/b11d2a1789a149c69b2dd218b1d2a2a1.jpg?v=1677759609028",   
+    },
+
+    {
+    id: 5,
+    item: "Serum",
+    image: "https://cdn.glitch.global/7778ae7d-3ffe-4d82-8131-4e9dc22235e5/pexels-ron-lach-8131586.jpg?v=1677759567302",   
+    },
+
+    {
+    id: 6,
+    item: "Face Tonic",
+    image: "https://cdn.glitch.global/7778ae7d-3ffe-4d82-8131-4e9dc22235e5/pexels-ron-lach-8128069.jpg?v=1677759573409",   
+    }
+]
